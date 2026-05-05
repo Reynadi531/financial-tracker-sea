33143afc7657fd30d1ccadd62754495b75e6cf7b
Reynadi Andreas Priesley <rey@reynadi.com>
2026-05-05 19:40:27 +0700

    feat: changing the logo when collapsed sidebar

--- apps/web/src/components/logo.tsx
+++ apps/web/src/components/logo.tsx
@@ -1,9 +1,14 @@
 import FtsLogo from "../assets/fts.svg";
+import FtsShortLogo from "../assets/fts-short.svg";
 
 export function Logo({ className = "", collapsed = false }: { className?: string; collapsed?: boolean }) {
   return (
     <div className={`flex items-center ${className}`}>
-      <img src={FtsLogo} alt="FTS Logo" className={`shrink-0 transition-all duration-300 ${collapsed ? "w-[60px] h-auto" : "w-[140px] h-auto"}`} />
+      <img
+        src={collapsed ? FtsShortLogo : FtsLogo}
+        alt="FTS Logo"
+        className={`shrink-0 transition-all duration-300 ${collapsed ? "w-[40px] h-auto" : "w-[140px] h-auto"}`}
+      />
     </div>
   );
 }
